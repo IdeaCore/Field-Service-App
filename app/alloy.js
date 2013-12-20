ac8d0588407a51fdf422a5c032be4d3d5c1b4582
Alan Leard <aleard@appcelerator.com>
2013-12-20 11:11:50 -0800

    Update alloy.js
    Update for new ti.map module

--- app/alloy.js
+++ app/alloy.js
@@ -1,3 +1,17 @@
+if (OS_ANDROID) {
+        Ti.Map = require('ti.map');
+
+// use the module on iOS with TiSDK 3.2.0+
+} else if (OS_IOS) {
+        var parts = Ti.version.split('.'),
+                major = parseInt(parts[0], 10),
+                minor = parseInt(parts[1], 10);
+
+        if (major > 3 || (major === 3 && minor >= 2)) {
+                Ti.Map = require('ti.map');
+        }
+}
+
 var APP = {
 	/**
 	 * Holds data from the JSON config file
